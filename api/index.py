12325a4db77bbda994524ceec6ac2f9dc7bcc679
weinymarchelina <weinymarchelina31@gmail.com>
2023-07-28 20:05:07 +0800

    spotify auth done

--- api/index.py
+++ api/index.py
@@ -1,6 +1,131 @@
-from flask import Flask
+import json
+import logging
+import os
+import time
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from flask import Flask, request, url_for, session, redirect
+from dotenv import load_dotenv
+
+load_dotenv()
+
 app = Flask(__name__)
+
+app.config['SESSION_COOKIE_NAME'] = "Spotify Cookie"
+app.secret_key = os.getenv("SECRET_KEY")
+TOKEN_INFO = "token_info"
+
+@app.route("/api")
+def login():
+    print("hoedy")
+    auth_url = create_spotify_oauth().get_authorize_url()
+    return redirect(auth_url)
+
+@app.route("/api/redirect")
+def redirect_page():
+    print("hey\n\n")
+    session.clear()
+    code = request.args.get("code")
+    token_info = create_spotify_oauth().get_access_token(code)
+    session[TOKEN_INFO] = token_info
+    return redirect(url_for('get_current_track', _external=True))
+
+@app.route("/api/getCurrentTrack")
+def get_current_track():
+    try: 
+        token_info = get_token()
+    except:
+        print('User not logged in')
+        return redirect("/")
+    
+    sp = spotipy.Spotify(auth=token_info['access_token'])
+
+    print("test main")
+    current_playing_track =  sp.current_user_playing_track()
+    album_info = current_playing_track['item']['album']
+    artist_info = current_playing_track['item']['artists']
+
+    print(current_playing_track)
+
+    """
+    current_playing_track_display = {
+        'id': current_playing_track['id'],
+        'name': current_playing_track['name'],
+        'album_id': album_info['id'],
+        'album_img': album_info['images'],
+        'album_name': album_info['name'],
+        'artist_id': artist_info['id'],
+        'artist_name': artist_info['name']
+    }
+
+    print(current_playing_track_display)
+    """
+
+    return ("User's current track is returned successfully.")
+
+
+@app.route("/api/getCurrentLyric")
+
+@app.route("/api/getLibrary")
+def get_library():
+    try: 
+        token_info = get_token()
+    except:
+        print('User not logged in')
+        return redirect("/")
+    
+    sp = spotipy.Spotify(auth=token_info['access_token'])
+
+    print("test aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+    user_playlists = sp.current_user_playlists(limit=5)
+    # playlist_display_data = []
+
+    for playlist in user_playlists['items']:
+        selected_playlist = {
+            'id': playlist['id'],
+            'images': playlist['images'],
+            'name': playlist['name'],
+            'tracks': playlist['tracks']
+        }
+        print(selected_playlist)
+        # playlist_display_data.append(selected_playlist)
+
+    return "User's library is returned successfully."
+
+    """
+    user_playlists =  sp.current_user_playlists()
+    user_saved_albums = sp.current_user_saved_albums()
+    # user_saved_tracks = sp.current_user_saved_tracks()['items']
+
+    print(user_playlists)
+    print(user_saved_albums)
+    """
+
+    return ("User's library is returned successfully.")
+
 
 @app.route("/api/python")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+def get_token():
+    token_info = session.get(TOKEN_INFO, None)
+    if not token_info:
+        redirect(url_for('login', _external=False))
+
+    current_time = int(time.time())
+
+    is_expired = token_info['expires_at'] - current_time < 60
+    if (is_expired):
+        spotify_oauth = create_spotify_oauth()
+        token_info = spotify_oauth.refresh_access_token(token_info['refresh_token'])
+
+    return token_info
+
+def create_spotify_oauth():
+    return SpotifyOAuth(
+        client_id=os.getenv("CLIENT_ID"),
+        client_secret=os.getenv("CLIENT_SECRET"),
+        redirect_uri=url_for("redirect_page", _external=True),
+        scope="user-library-read user-read-currently-playing user-modify-playback-state playlist-read-private playlist-read-collaborative user-top-read"
+    )
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -7,8 +7,8 @@ export default function Home() {
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
-          <Link href="/api/python">
-            <code className="font-mono font-bold">api/index.py</code>
+          <Link href="/api/">
+            <code className="font-mono font-bold">Login</code>
           </Link>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
@@ -18,7 +18,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -50,7 +50,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
+            Docs{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -67,7 +67,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
+            Learn{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -84,7 +84,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
+            Templates{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -101,7 +101,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
+            Deploy{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -112,5 +112,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
